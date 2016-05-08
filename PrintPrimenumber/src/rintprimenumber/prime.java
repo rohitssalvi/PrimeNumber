0a15deb2dfdd7d4d02c3b2086d4c356233019555
rohitssalvi <rsalvi923@gmail.com>
2016-05-08 08:14:31 +0530

    Added files via upload

---
+++ PrintPrimenumber/src/rintprimenumber/prime.java
@@ -0,0 +1,30 @@
+package rintprimenumber;
+
+public class prime {
+	public static void main(String[] args) {
+		
+		int i;
+		
+		for(i=1;i<=100;i++)
+		{
+			int counter=0;
+			for(int num=i;num>=1;num--)
+			{
+				if(i%num==0)
+				{
+				counter=counter+1;
+				}
+				
+				
+			}
+			if(counter==2)
+			{
+			    System.out.println(i);	
+			}
+			
+		}
+		
+		
+	}
+
+}
